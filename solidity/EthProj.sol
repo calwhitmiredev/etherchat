9035444d9dbc14edd6fec493ada241dc09e8b86d
calvinodev <calwhitmiredev@outlook.com>
2018-05-19 09:33:58 -0400

    Update
    Update

--- solidity/EthProj.sol
+++ solidity/EthProj.sol
@@ -4,17 +4,17 @@ contract EthProj {
     
     string fMessage;
     
-    event Message(string message, address add, uint cost);
-    event Username(string name, address add, uint cost);
+    event Message(string message, address add, uint256 cost);
+    event Username(string name, address add, uint256 cost);
    
     function setMessage(string _fMessage) public {
         fMessage = _fMessage;
         
-        emit Message(_fMessage, msg.sender, tx.gasUsed);
+        emit Message(_fMessage, msg.sender, gasleft());
     }
     
     function setUsername(string _userName) public {
-        emit Username(_userName, msg.sender, tx.gasUsed);
+        emit Username(_userName, msg.sender, gasleft());
     }
    
 }
